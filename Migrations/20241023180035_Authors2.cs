fae66e1e3ec7b6610a6c976de81887f6d1a19fb6
Andrei Filip <andrei.stefan.filip@gmail.com>
2024-10-23 22:40:56 +0300

    Add project files.

---
+++ Migrations/20241023180035_Authors2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filip_Andrei_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Authors2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Author",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LastName",
+                table: "Author",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Filip_Andrei_Lab2.Data;
+using Filip_Andrei_Lab2.Models;
+
+namespace Filip_Andrei_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Filip_Andrei_Lab2.Data.Filip_Andrei_Lab2Context _context;
+
+        public IndexModel(Filip_Andrei_Lab2.Data.Filip_Andrei_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+            .Include(b => b.Author)
+            .Include(b=>b.Publisher)
+            .ToListAsync();
+        }
+    }
+}
